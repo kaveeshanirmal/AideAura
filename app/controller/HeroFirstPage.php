3bf21f442fab12243660edbe223b56428c03109d
Kooni Hewage Chandunu Maneth De Silva <2022cs032@stu.ucsc.cmb.ac.lk>
2024-11-15 14:18:33 +0530

    Add files via upload
    Added files with .php extension

---
+++ app/controller/HeroFirstPage.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" with="device-width , initial-scale=1.0">
+    <title>AideAura</title>
+    <link rel="stylesheet" href="../../public/assets/css/HeroFirstPage.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Montserrat:wght@100&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap');
+    </style>
+</head>
+
+<body>
+
+    <!-- First Page -->
+    <section class="page-1">
+        <div class="header-1">
+            <h1>Where Quality Care<br>Meets Home!</h1>
+        </div>  
+        <div class="subheader">
+            <h2>
+                Experience trustworthy care for all your household work<br>with the best quality and reasonable prices
+            </h2>
+        </div>
+        <button class="glow-button">Find a Worker</button>
+        <div class="background-image">
+            <img src="../../public/assets/images/Hero_Screen_1_Background.png" class="background-image" alt="background">
+        </div>
+    </section>
+
+    <!-- Second Page -->
+    <section class="page-2">
+        <div class="background-image">
+            <img src="../../public/assets/images/Hero_Slider_1.png" class="background-image" alt="background">
+            <img src="../../public/assets/images/Loading_Screen_Background_Front_Layer.png" class="background-layer" alt="bg-layer">
+        </div>
+        <div class="container">
+            <div id="slide">
+                <!-- Slide 1 -->
+                <div class="item" style="background-image: url(../../public/assets/images/Hero_Slider_1.png);">
+                    <div class="content">
+                        <div class="name">Babysitting</div>
+                        <div class="description">
+                            AideAura offers experienced and trustworthy babysitters to ensure your child is in safe hands.<br>Our babysitters are trained in child care, including feeding, engaging in educational activities,<br>and maintaining a secure environment. Whether for a few hours or full-time care, our<br>babysitters adapt to your family's needs.
+                        </div>
+                    </div>
+                </div>
+                <!-- Slide 2 -->
+                <div class="item" style="background-image: url(../../public/assets/images/Hero_Slider_2.png);">
+                    <div class="content">
+                        <div class="name">Cooking</div>
+                        <div class="description">
+                            AideAura provides skilled cooks who can prepare meals according to your dietary preferences and <br>cultural cuisine. From daily meals to special occasions, our cooks ensure quality, hygiene, and <br>deliciousness in every dish. Enjoy the convenience of home-cooked meals without the <br>stress of preparation.
+                        </div>
+                    </div>
+                </div>
+                <!-- Slide 3 -->
+                <div class="item" style="background-image: url(../../public/assets/images/Hero_Slider_3.png);">
+                    <div class="content">
+                        <div class="name">Cleaning</div>
+                        <div class="description">
+                            Our professional cleaners at AideAura guarantee a spotless and tidy home. Trained in efficient <br>cleaning techniques, our staff handles everything from deep cleaning to daily upkeep,<br>ensuring your home remains organized and welcoming. We offer flexible schedules<br>tailored to your lifestyle.
+                        </div>
+                    </div>
+                </div>
+                <!-- Slide 4 -->
+                <div class="item" style="background-image: url(../../public/assets/images/Hero_Slider_4.png);">
+                    <div class="content">
+                        <div class="name">Gardening</div>
+                        <div class="description">
+                            AideAura's gardening services help maintain the beauty of your outdoor space. From routine lawn <br>care to plant maintenance, our gardeners take care of all aspects of your garden, keeping it lush <br> and vibrant. We ensure your garden stays healthy and enjoyable year-round.
+                        </div>
+                    </div>
+                </div>
+                <!-- Slide 5 -->
+                <div class="item" style="background-image: url(../../public/assets/images/Hero_Slider_5.png);">
+                    <div class="content">
+                        <div class="name">Housekeeping</div>
+                        <div class="description">
+                            AideAura's housekeeping services provide comprehensive home management. Our housekeepers<br>are trained to handle laundry, organizing, and maintaining a well-ordered home environment.<br>Whether you need part-time or full-time assistance, we customize our services to meet<br>your specific requirements.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="buttons">
+                <button id="prev"><img src="../../public/assets/images/Prev_Icon.png" alt="prev_button"></button>
+                <button id="next"><img src="../../public/assets/images/Next_Icon.png" alt="next_button"></button>
+            </div>
+        </div>
+    
+        <script src="../../public/assets/js/HeroSectionScript.js"></script>
+        
+    </section>
+
+    <!-- Third Page -->
+    <section class="page-3">
+        <div class="background-image">
+            <img src="../../public/assets/images/Hero_Screen_3_Background.png" class="background-image" alt="background">
+        </div>
+    </section>
+
+</body>
+</html>
